4a81d7279a184ac9fd6ff9050fa5110ddcf41c07
Rik Kierkels <Kierkels.r@gmail.com>
2020-02-15 13:11:24 +0100

    encapsulate the handlers for a specific client

--- server/handlers.js
+++ server/handlers.js
@@ -1,13 +1,26 @@
+'use strict';
 import UserService from './user-service';
 
-export function handleRegister(user, clientId, callback) {
-  try {
-    callback(null, UserService.register(user, clientId));
-  } catch (e) {
-    callback(e.message);
+export function makeHandlers(client) {
+  function handleRegister(user, callback) {
+    try {
+      callback(null, UserService.register(user, client.id));
+    } catch (e) {
+      callback(e.message);
+    }
   }
-}
 
-export function handleDisconnect(clientId) {
-  UserService.unregister(clientId);
+  function handleMessage(message, callback) {
+    console.log(message);
+  }
+
+  function handleDisconnect() {
+    UserService.unregister(client.id);
+  }
+
+  return {
+    handleRegister,
+    handleMessage,
+    handleDisconnect
+  };
 }
