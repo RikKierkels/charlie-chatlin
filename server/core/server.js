d172904cfe56c46c6a1e92b9c96337a4dcce4ba0
Rik Kierkels <Kierkels.r@gmail.com>
2020-02-16 14:57:27 +0100

    add express to handle requests

--- server/core/server.js
+++ server/core/server.js
@@ -1,13 +1,19 @@
 'use strict';
-import chalk from 'chalk';
-import io from 'socket.io';
+import express from 'express';
 import * as http from 'http';
+import io from 'socket.io';
+import chalk from 'chalk';
 import { makeHandlers } from './handlers';
 
 const PORT = 3000;
 const log = console.log;
-const server = http.createServer();
+const app = express();
+const server = http.createServer(app);
 const socket = io(server);
+
+app.get('/vapid', (req, res) => {
+  res.send('working');
+});
 
 socket.on('connection', client => {
   log(`client connected... ${chalk.red(client.id)}`);
