b3bd902b31dff49d0b0c49ed706910254fd7b832
Rik Kierkels <Kierkels.r@gmail.com>
2020-02-18 21:33:20 +0100

    change the theme color

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   pwa: {
     name: 'Charlie Chatlin',
-    themeColor: '#4DBA87',
+    themeColor: '#6000FF',
     msTileColor: '#000000',
-
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       swSrc: 'public/service-worker.js'
-      // ...other Workbox options...
     }
   }
 };
