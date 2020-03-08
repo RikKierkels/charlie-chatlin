d05280628a98bd9d71b9239835302c997f6ead39
Rik Kierkels <Kierkels.r@gmail.com>
2020-03-08 11:39:12 +0100

    throw specific errors when validation fails

--- server/src/utils/validator.js
+++ server/src/utils/validator.js
@@ -1,41 +1,65 @@
 const joi = require('@hapi/joi');
 
+function toStringValidationMessages(field, max) {
+  return {
+    'string.base': `${field} should be a type of 'text.'`,
+    'string.empty': `${field} cannot be an empty field.`,
+    'string.max': `${field} has a maximum length of ${max}.`,
+    'any.required': `${field} is a required field.`
+  };
+}
+
+function toNumberValidationMessages(field) {
+  return {
+    'number.base': `${field} is not a valid number or could not be cast to a valid number.`,
+    'number.integer': `${field} is not a valid integer.`
+  };
+}
+
 const messageSchema = joi
   .string()
   .trim()
   .max(1000)
-  .required();
+  .required()
+  .messages(toStringValidationMessages('Message', 1000));
 
 const userSchema = joi.object({
   username: joi
     .string()
     .trim()
     .max(100)
-    .required(),
+    .required()
+    .messages(toStringValidationMessages('Username', 100)),
   avatarId: joi
     .string()
     .trim()
     .max(250)
+    .required()
+    .messages(toStringValidationMessages('AvatarId', 250))
 });
 
 const subscriptionSchema = joi.object({
   endpoint: joi
     .string()
     .max(2048)
-    .required(),
+    .required()
+    .messages(toStringValidationMessages('Endpoint', 2048)),
   expirationTime: joi
     .number()
     .integer()
-    .allow(null),
+    .allow(null)
+    .messages(toNumberValidationMessages('ExpirationTime')),
   keys: joi.object({
     p256dh: joi
       .string()
       .max(250)
-      .required(),
+      .required()
+      .messages(toStringValidationMessages('P256dh', 250)),
     auth: joi
       .string()
       .max(200)
       .required()
+      .messages(toStringValidationMessages('Auth', 200))
   })
 });
 
