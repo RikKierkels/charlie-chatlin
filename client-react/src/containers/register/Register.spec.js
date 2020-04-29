030bcb40b583d93a9607c9fca96cdc19b90e539c
Rik Kierkels <kierkels.r@gmail.com>
2020-04-29 07:23:28 +0200

    Add missing test case for no avatar and username selected

--- client-react/src/containers/register/Register.spec.js
+++ client-react/src/containers/register/Register.spec.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import Register from './register';
-import { renderWithTheme } from '../../test-utils';
+import { renderWithReduxAndTheme } from '../../test-utils';
 import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+const renderRegisterContainer = () => {
+  const history = createMemoryHistory();
+  return {
+    ...renderWithReduxAndTheme(
+      <Router history={history}>
+        <Register />
+      </Router>,
+    ),
+    history,
+  };
+};
 
 const usernameInput = () => screen.getByRole('textbox');
 const registerButton = () => screen.getByRole('button', { name: /register/i });
@@ -12,29 +26,37 @@ const randomAvatar = () => {
 };
 
 test('registers a user', () => {
-  renderWithTheme(<Register />);
+  const { history } = renderRegisterContainer();
 
   userEvent.click(randomAvatar());
   userEvent.type(usernameInput(), 'Dog');
   userEvent.click(registerButton());
 
-  expect(true);
+  expect(history.location.pathname).toBe('/chat');
+});
+
+test('cannot register without selecting an avatar and a username', () => {
+  const { history } = renderRegisterContainer();
+
+  userEvent.click(registerButton());
+
+  expect(history.location.pathname).not.toBe('/chat');
 });
 
 test('cannot register without selecting an avatar', () => {
-  renderWithTheme(<Register />);
+  const { history } = renderRegisterContainer();
 
   userEvent.type(usernameInput(), 'Dog');
   userEvent.click(registerButton());
 
-  expect(true);
+  expect(history.location.pathname).not.toBe('/chat');
 });
 
 test('cannot register without entering a username', () => {
-  renderWithTheme(<Register />);
+  const { history } = renderRegisterContainer();
 
   userEvent.click(randomAvatar());
   userEvent.click(registerButton());
 
-  expect(true);
+  expect(history.location.pathname).not.toBe('/chat');
 });
