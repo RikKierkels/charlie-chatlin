2a4b0330ab23f1e1ca19a473d7548af61a7d41d4
Rik Kierkels <kierkels.r@gmail.com>
2020-04-30 08:13:48 +0200

    Use the real store for tests

--- client-react/src/store/userSlice.js
+++ client-react/src/store/userSlice.js
@@ -8,7 +8,6 @@ const userSlice = createSlice({
   },
   reducers: {
     setUser: (state, action) => {
-      console.log(action);
       state.username = action.payload.username;
       state.avatar = action.payload.avatar;
     },
--- client-react/src/test-utils.js
+++ client-react/src/test-utils.js
@@ -2,11 +2,11 @@ import React from 'react';
 import theme from './design/theme';
 import { ThemeProvider } from 'styled-components';
 import { render } from '@testing-library/react';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import store from './store/store.js';
 
 export const renderWithTheme = (ui) => render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>);
 
-export const renderWithReduxAndTheme = (ui, { initialState = {}, store = createStore(null, initialState) } = {}) => {
+export const renderWithReduxAndTheme = (ui) => {
   return renderWithTheme(<Provider store={store}>{ui}</Provider>);
 };
