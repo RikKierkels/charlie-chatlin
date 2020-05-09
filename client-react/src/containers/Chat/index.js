ed7868d3463786d9864bf5b0c861d89ecff96545
Rik Kierkels <kierkels.r@gmail.com>
2020-05-09 22:18:21 +0200

    Add a minimal version of the user joined tile

--- client-react/src/containers/Chat/index.js
+++ client-react/src/containers/Chat/index.js
@@ -8,8 +8,11 @@ import chat from '../../shared/chat';
 import { useSelector } from 'react-redux';
 import { MESSAGE_TYPE } from '../../shared/socket-constants';
 import TileMessage from '../../components/TileMessage';
+import TileUserJoined from '../../components/TileUserJoined';
 
-const toMessageTile = (message) => (message.type === MESSAGE_TYPE.TEXT ? <TileMessage message={message} /> : null);
+const toMessageTile = (message) => {
+  return message.type === MESSAGE_TYPE.TEXT ? <TileMessage message={message} /> : <TileUserJoined message={message} />;
+};
 
 const Chat = () => {
   const users = useSelector((state) => state.chat.users);
