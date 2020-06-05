ac456e1d1c5f7b3a11e70132035621062a697dc8
Rik Kierkels <kierkels.r@gmail.com>
2020-06-05 16:10:06 +0200

    Update gitignore

--- client-react/src/components/TileUserJoined/index.js
+++ client-react/src/components/TileUserJoined/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import styled from '@emotion/styled';
 import Tile from '../Tile';
 
 const TileUserJoined = ({ message }) => {
+  const fetchGif = useCallback(async () => {
+    const url = new URL('https://api.giphy.com/v1/gifs/random');
+    url.searchParams.append('api_key', process.env.REACT_APP_GIPHY_API_KEY);
+    url.searchParams.append('tag', 'applause');
+
+    const data = await fetch(url);
+    console.log(data);
+  }, []);
+
+  useEffect(() => fetchGif, [fetchGif]);
+
   return <StyledTile>{message.text}</StyledTile>;
 };
 
