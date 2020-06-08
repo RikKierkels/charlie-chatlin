3f2012d78288ad4c8b6c99eee94e577afc79e246
Rik Kierkels <kierkels.r@gmail.com>
2020-06-08 07:51:47 +0200

    Remove obsolete user joined test

--- client-react/src/components/TileUserJoined/TileUserJoined.spec.js
+++ client-react/src/components/TileUserJoined/TileUserJoined.spec.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/dom';
+import { screen } from '@testing-library/dom';
 import { renderWithTheme } from '../../test-utils';
 import TileUserJoined from './index';
 
 test('has message text', () => {
   const text = 'Tabs has joined the room!';
-  fetch.mockResponse(JSON.stringify({ data: {} }));
+  fetch.mockResponseOnce(JSON.stringify({ data: {} }));
 
   renderWithTheme(<TileUserJoined message={{ text }} />);
 
   expect(screen.getByText(text)).toBeInTheDocument();
-});
-
-test('has a gif as background', async () => {
-  const gif = 'https://www.giphy.com/applause.gif';
-  fetch.mockResponse(JSON.stringify({ data: { images: { ['downsized_medium']: { url: gif } } } }));
-
-  const { container } = renderWithTheme(<TileUserJoined message={{ text: '' }} />);
-
-  await waitFor(() => expect(container.firstElementChild).toHaveStyleRule('background-image', `url("${gif}")`));
 });
